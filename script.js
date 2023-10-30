934e09a6e877451b85ae20eff417cfb67267cc55
Thomas Suggitt <tmjs2001@gmail.comgit config --global user.name TSuggitt>
2023-10-30 16:35:49 +0000

    fetch working

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fruitform.addEventListener("submit", extractFruit)
 
 function extractFruit(e){
     e.preventDefault()       
-    addFruit(e.target[0].value)
+    fetchFruitData(e.target[0].value) 
     e.target[0].value = ""
 }
 
@@ -14,22 +14,63 @@ function extractFruit(e){
 
 // now we have extracted the submission
 
+const fruitNutrition = document.querySelector("#nutritionSection p")
+
+function fetchFruitData(fruit){
+    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+    .then(processResponse)
+    .then(data => addFruit(data))
+    .catch((err) =>console.log(err))
+                              //we take data - need to convert from json back to js code 
+
+}
+function processResponse(resp){
+    if(resp.ok){
+        return resp.json()
+    }
+    else{
+        throw "Error: http status code = "+ resp.status
+    }
+}
+
+
 const fruitList = document.querySelector("#fruitSection ul")
+
+let calories = 0; 
+let fruitCal = {}
 
 function addFruit(fruit){
     if(!fruit){
         console.log('Invalid Fruit')
     } else {
-    const li = document.createElement("li");
-    li.addEventListener("click", removeFruit, {once:true}) //creates new li+ option to remove  - //once:true makes sure the event listener doesnt wait around for it as the list item has been deleted
-    li.textContent = fruit  // li is set to fruit 
+    const li = document.createElement("li"); // adds a new list item
+    li.addEventListener("click", removeFruit, {once:true}) // option to remove  - //once:true makes sure the event listener doesnt wait around for it as the list item has been deleted
+    li.textContent = fruit['name']  // li is set to fruit 
     fruitList.appendChild(li) // appends li inside ul 
+    fruitCal[fruit.name] = fruit.nutritions.calories
+    calories += fruit.nutritions.calories
+    fruitNutrition.textContent = calories
+
+    //for deleting calories along with the fruit 
+    // -
+
 }}
+
 
 //every time addFruit is called, we add a new item to the unordered list which is the fruit which has been submitted
 
 // ** add Fruit is then used with extracFruit to add the submitted fruit to the DOM/ List in HTML 
 
 function removeFruit(e){
+    const fruitName = e.target.textContent
+    calories -= fruitCal[fruitName]
+    fruitNutrition.textContent = calories
+
+    delete fruitCal[fruitName]
+
     e.target.remove()
+    
 }
+
+
+ 

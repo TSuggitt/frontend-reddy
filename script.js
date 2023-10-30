b2f457f3adc3b237684234f729c1389bb90cfb23
Thomas Suggitt <tmjs2001@gmail.comgit config --global user.name TSuggitt>
2023-10-30 11:48:55 +0000

    form added

---
+++ script.js
@@ -0,0 +1,5 @@
+const fruitform = document.querySelector("#inputSection form")
+console.log(fruitform)
+
+const david = require("./script2")
+console.log(david)
---
+++ script2.js
@@ -0,0 +1,3 @@
+const x = 'david'
+
+module.exports = x 
